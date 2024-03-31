3fcd15b8ca07f03c8c41834f25a9808ad814785b
Sergey Melnik <ic-energo@ya.ru>
2024-04-01 02:30:50 +0800

    add click

--- main.py
+++ main.py
@@ -16,12 +16,20 @@ def main_menu():
     while running:
         screen.fill((0, 0, 0))
 
+        font = pygame.font.Font(None, 72)
+        text_surface = font.render("BUTTON TEST", True, (255, 255, 255))
+        text_rect = text_surface.get_rect(center=(300, 50))
+        screen.blit(text_surface, text_rect)
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
                 pygame.quit()
                 sys.exit()
 
+            green_button.handle_event(event)
+
+        green_button.check_hover(pygame.mouse.get_pos())
         green_button.draw(screen)
         pygame.display.flip()
 
